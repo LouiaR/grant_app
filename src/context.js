b98ef46422f5428871982b22cb737dd400944c21
louiAr <louitiani@gmail.com>
2018-10-30 09:41:30 +0000

    Add context api

---
+++ src/context.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+    state = {
+        grants_list: []
+    }
+    
+  async componentDidMount() {
+    try {
+      const response = await axios.get(process.env.REACT_APP_URL);
+      this.setState({
+        grants_list: response.data
+      })
+    } catch (err) {
+      console.log(err)
+    } 
+  } 
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    )
+  }  
+}
+
+export const Consumer = Context.Consumer;
