cb16c1964edae58e525fe051dd262f75ba0ef265
louiAr <louitiani@gmail.com>
2018-10-30 08:26:49 +0000

    Add top header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+const header = () => {
+  return (
+    <header>
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="/">
+          <span>Comic Relief Grants</span>
+        </a> 
+      </nav>
+    </header>
+  );
+};
+
+export default header;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/aux";
+import Header from "./Header";
+
 
 const layout = props => {
   return (
     <Aux>
+    <Header />
       <main className="container-fluid">
         <h1>Promo</h1>
         {props.children}
